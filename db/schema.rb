e73b47509c0a1a0b122fcfb07b152e543d0751da
prot-dylan <maloney.dylan@gmail.com>
2020-05-26 14:13:41 -0400

    Basic Post scaffold working

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_05_26_180922) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "name"
+    t.string "title"
+    t.text "contents"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
